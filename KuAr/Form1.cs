98d2f271145cd1ef1feee7f521593aee7d568d71
chertik14555 <145104929+chertik14555@users.noreply.github.com>
2024-05-31 08:41:36 +0300

    Add files via upload

---
+++ KuAr/Form1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MessagingToolkit.QRCode.Codec;
+using MessagingToolkit.QRCode.Codec.Data;
+using Word = Microsoft.Office.Interop.Word;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+using Microsoft.Office.Interop.Word;
+
+namespace KuAr
+{
+    public partial class Form1 : Form
+    {
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string text=textBox1.Text;
+            QRCodeEncoder encoder = new QRCodeEncoder();
+            Bitmap bitmap = encoder.Encode(text);
+            pictureBox1.Image = bitmap as Image;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            SaveFileDialog save = new SaveFileDialog(); 
+            save.Filter = "PNG|*.png|JPEG|*.jpg|GIF|*.gif|BMP|*.bmp"; 
+            if (save.ShowDialog() == System.Windows.Forms.DialogResult.OK) 
+            {
+                pictureBox1.Image.Save(save.FileName); 
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog load = new OpenFileDialog();
+            if (load.ShowDialog() == System.Windows.Forms.DialogResult.OK) 
+            {
+                pictureBox1.ImageLocation = load.FileName; 
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            QRCodeDecoder decoder = new QRCodeDecoder();
+            MessageBox.Show(decoder.decode(new QRCodeBitmapImage(pictureBox1.Image as Bitmap))); 
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            Word.Application wordApp = new Word.Application();
+            Word.Document doc = wordApp.Documents.Add();
+
+            Word.HeaderFooter header = doc.Sections[1].Headers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary];
+            Word.Range range = header.Range;
+
+            Word.HeaderFooter footer = doc.Sections[1].Footers[Word.WdHeaderFooterIndex.wdHeaderFooterPrimary];
+            Word.Range footerRange = footer.Range;
+
+            string tempImagePath = System.IO.Path.GetTempPath() + "tempImage.jpg";
+            pictureBox1.Image.Save(tempImagePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+
+            footerRange.InlineShapes.AddPicture(tempImagePath);
+
+            System.IO.File.Delete(tempImagePath);
+
+            wordApp.Visible = true;
+        }
+    }
+}
